f45cfabecc1c10e90a22167806466652337c4298
Josh Powell <josh.powell@softwire.com>
2019-06-25 11:33:14 +0100

    connect database

---
+++ api/src/database-service.js
@@ -0,0 +1,22 @@
+import db from 'mysql-promise';
+
+export default class databaseService{
+
+init(){
+
+const serverName = 'yuglu.cmvjpciisuyc.eu-west-1.rds.amazonaws.com';
+const userName = 'joshPowell';
+const password = 'password1234';
+const databaseSchema = 'Yuglu';
+const database = new db()
+database.configure({
+    "host": serverName,
+    "user": userName,
+    "password": password,
+    "database": databaseSchema
+});
+
+
+return database
+}
+}
--- api/src/helloWorld-controller.js
+++ api/src/helloWorld-controller.js
@@ -1,8 +1,11 @@
+import db from './database-service';
+
 export default class HelloWorld {
 
 static register(app) {
 
-
+    var blah = new db();
+    blah.init();
     app.get('/', function(req, res) {
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify({ data: "hello world" }));
