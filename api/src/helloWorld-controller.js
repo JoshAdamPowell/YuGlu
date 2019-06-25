7e2a04d3ef3e0d25fab2f4e3d3102b971ac76c32
Josh Powell <josh.powell@softwire.com>
2019-06-25 16:54:31 +0100

    Update code

--- api/src/helloWorld-controller.js
+++ api/src/helloWorld-controller.js
@@ -6,7 +6,7 @@ static register(app) {
 
     var blah = new db();
     blah.init();
-    app.get('/', function(req, res) {
+    app.get('/helloWorld', function(req, res) {
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify({ data: "hello world" }));
     });
--- api/src/index.js
+++ api/src/index.js
@@ -6,15 +6,16 @@ init(){
     // SETUP EXPRESS
     const app = express();
     app.use(express.urlencoded());
-
-    const port = 3000;
+    app.set('port', 3001);
+console.log('blah')
+console.log(app.get('port'));
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+    const port = 3001;
 
     HelloWorld.register(app);
-
-
-
-
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    //app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 }
     
