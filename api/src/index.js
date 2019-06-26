6409f7b8cfd7a80c04d6df2ef761ed4d9b676866
Jay Khetia <Jkhets@gmail.com>
2019-06-26 14:52:35 +0100

    sign up back end 1.1
    reformatted, followed all of josh's suggestions. i can finally rest now.

--- api/src/index.js
+++ api/src/index.js
@@ -1,39 +1,30 @@
 import express from 'express';
 import HelloWorld from './controllers/helloWorld-controller';
-import signUpController from './controllers/sign-up-controller'
+import signUpController from './controllers/sign-up-controller';
 
 class App {
-
-    
-
-init(){
+  
+  init() {
     // SETUP EXPRESS
     const app = express();
     app.use(express.urlencoded());
     app.set('port', 3001);
-console.log('blah')
-console.log(app.get('port'));
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
-});
-    const port = 3001;
-
-    const port = 3001;
+    console.log('blah')
+    console.log(app.get('port'));
+    var server = app.listen(app.get('port'), function () {
+      console.log('Express server listening on port ' + server.address().port);
+    });
+    const port = 3000;
+    
 
     HelloWorld.register(app);
     signUpController.register(app);
-
-
-
-    if (!this.setup){
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    
+    if (!this.setup) {
+      app.listen(port, () => console.log(`Example app listening on port ${port}!`));
     }
     this.setup = true;
+  }
 }
-    
-}
-
-
-
 const application = new App()
 application.init();
--- api/src/services/users-service.js
+++ api/src/services/users-service.js
@@ -1,28 +1,23 @@
-import databaseService from './database-service';
+import databaseService from '../services/database-service';
 import bcrypt from 'bcrypt';
 
-export default class signUpUser{
+export default class userService {
 
-static signUpUser(userName,password,firstName,lastName,email){
-const db =  new databaseService()
-    const connection = db.init()
-    var saltRounds = 10;
+    static signUpUser(userName, password, firstName, lastName, email) {
+        const db = new databaseService()
+        const connection = db.init()
+        var saltRounds = 10;
 
-    bcrypt.hash(password,saltRounds, function(err,hash){
-    const query = 'INSERT INTO Users (userName,password,firstName,lastName,email) VALUES (?,?,?,?,?)'
-    const parameters = [userName,hash,firstName,lastName,email];
-    
-    connection.query(query, parameters, function (error, results, fields) {
-        if (error) throw error;
-        callback();
-    });
+        bcrypt.hash(password, saltRounds, function (err, hash) {
+            const query = 'INSERT INTO Users (userName,password,firstName,lastName,email) VALUES (?,?,?,?,?)'
+            const parameters = [userName, hash, firstName, lastName, email];
 
-})
-
-
-
-
-}
+            connection.query(query, parameters, function (error, results, fields) {
+                if (error) throw error;
+                callback();
+            });
+        })
+    }
 }
 
 
