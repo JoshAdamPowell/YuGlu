fa9cb216db71aa6febc997e183a2bcbcbfcc5fdc
Charlie Quin <charliequin@outlook.com>
2019-06-28 14:01:37 +0100

    Editing login back-end

--- yuglu-frontend/src/App.js
+++ yuglu-frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import SignUp from './components/sign-up/sign-up';
+import SignUp from './components/SignUp/sign-up';
 import Login from './components/Login/Login';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Home from './components/home/Home'
-import Dashboard from './components/dashboard/Dashboard'
-import AddPost from './components/addpost/AddPost'
-import NavBar from './components/navbar/NavBar' 
+import Home from './components/Home/Home'
+import Dashboard from './components/Dashboard/Dashboard'
+import AddPost from './components/AddPost/AddPost'
+import NavBar from './components/Navbar/NavBar' 
 
 function App() {
   console.log(process.env.PORT)
@@ -14,6 +14,7 @@ function App() {
     <Router>
       <NavBar></NavBar>
       <Route exact path="/" component={Home} />
+      <Route path="/Login" component={Login} />
       <Route path="/signup" component={SignUp} />
       <Route path="/dashboard" component={Dashboard} />
       <Route path="/addnew" component={AddPost} /> 
