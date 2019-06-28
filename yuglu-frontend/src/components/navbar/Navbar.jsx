977615ef101c2aeb68590b12e6d045f883ec3d97
Courtney Turner-Noakes <c7rd83631809@hotmail.com>
2019-06-28 13:33:01 +0100

    Auto stash before merge of "courtney" and "origin/master"

---
+++ yuglu-frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './src/navbar'
+
+const Navbar = props => {
+    <header className="navbar">
+        <nav>
+        <div className="sign_up"><a href="/sign-up">Sign-up</a></div>
+        </nav>
+    </header>
+}
+
+
+export default Navbar;
---
+++ yuglu-frontend/src/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export default class Navbar extends React.Component{
+    render() {
+        return (
+            <div>
+              <ul id="nav">
+                <li><a href="/sign-up">Signup</a></li>
+              
+              </ul>
+            </div>
+        );
+    }
+}
