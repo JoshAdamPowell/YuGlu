9b0d0fb4f6802eba910ccac11471b71618632399
Harri New <harriet.new@tesglobal.com>
2019-07-02 16:41:50 +0100

    for courtney

--- yuglu-frontend/src/App.js
+++ yuglu-frontend/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import SignUp from './components/sign-up/sign-up';
-import Navbar from '../src/navbar'
 import Login from './components/Login/Login';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './components/Home/Home'
 import Dashboard from './components/dashboard/Dashboard';
-import AddPost from './components/Addpost/addPost';
-import NavBar from './components/navbar/NavBar';
+import AddPost from './components/addpost/AddPost';
+import NavBar from './components/navbar/Navbar';
 
 
 function App() {
